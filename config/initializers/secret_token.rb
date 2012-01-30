4aec4ba76052a30e7c25d07bcbc5eadf24b5ff3d
sriram ilapakurty <sriram.in20@gmail.com>
2012-01-30 23:29:20 +0530

    myfirstcommit in git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuExm::Application.config.secret_token = 'e6a5dc90b72e308038bac3717bf1360d504f2c5400f7d098f29de6788cb339a5b3c283987b563eca7d3f7e7f9478901c0b23c2180664ece6d15df2999c87cf6b'
